266c9df60b6efd265298022c76dbd42f85d90314
Tchilo <tchiloross@gmail.com>
2022-04-26 18:00:33 +0200

    Create databasee, create api endpoints, create react component

---
+++ app/controllers/v1/greetings_controller.rb
@@ -0,0 +1,10 @@
+
+  
+class V1::GreetingsController < ApplicationController
+  def index
+    @greetings = Message.find(Message.pluck(:id).sample)
+    render json: {
+      greetings: @greetings
+    }.to_json
+  end
+end
